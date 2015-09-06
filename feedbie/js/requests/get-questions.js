c8c53a454321ce0cb3485703b80e1a5be64e55a3
Marzieh Morovatpasand <mmorova@thoughtworks.com>
2015-09-06 12:50:20 +1000

    no need anymore

--- feedbie/js/requests/get-questions.js
+++
@@ -1,9 +0,0 @@
-function getQuestions() {
-    $.get('http://hackathon-dev.elasticbeanstalk.com/lessons/api/lessons/1/')
-        .done(function(data) {
-            return data;
-        }).fail(function(err) {
-            console.log("OoopSS!!!, An error happened:", err);
-            return null;
-        });
-}
